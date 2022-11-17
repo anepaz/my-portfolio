7f5e0c3a426e8216ac2bd1b721b71a5398490901
anepaz <anelisespaz@gmail.com>
2022-11-16 23:36:36 -0300

    feat(header-styles): add header and style css

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,29 +3,24 @@ import "./header.css";
 const Header = () => {
   return (
     <header className="header">
-      <nav className="nav">
-        <div className="nav__menu">
+        <nav className="nav__menu">
           <ul className="nav__list">
             <li className="nav__item">
-              <a href="#home" className="nav__link">
-                <i className="icon-home">Home</i>
+              <a href="#home" className="nav__link">Home
               </a>
             </li>
 
             <li className="nav__item">
-              <a href="#contact" className="nav__link">
-                <i className="icon-user-following">Contact Me</i>
+              <a href="#contact" className="nav__link">Contact Me
               </a>
             </li>
 
             <li className="nav__item">
-              <a href="#skills" className="nav__link">
-                <i className="icon-book-open">Skills & Tools</i>
+              <a href="#skills" className="nav__link">Skills & Tools
               </a>
             </li>
           </ul>
-        </div>
-      </nav>
+        </nav>
     </header>
   );
 };
